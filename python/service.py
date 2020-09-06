9c5e78ae2f23369b42035ecf2b42abe1e95b6a3c
ValentinMorel <valntn.mrl@gmail.com>
2020-09-06 21:34:48 +0200

    Implement Block aggregation and Blockchain Creation.

---
+++ python/service.py
@@ -0,0 +1,17 @@
+import yagmail
+from nameko.rpc import rpc, RpcProxy
+
+class Blockchain(object):
+    name = 'blockchain'
+    self.chain = []
+    @rpc
+    # function to create blockchain
+    def create_blockchain(self, data):
+
+
+    # function to add block from data
+
+
+
+
+
